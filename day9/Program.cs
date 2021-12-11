edb85b5bc696e11c87a1e30378f1e05a08c40797
Andrew Douglas <potterboy67@outlook.com>
2021-12-11 17:40:06 +0000

    completed second part

--- day9/Program.cs
+++ day9/Program.cs
@@ -51,12 +51,21 @@ namespace day9
             }
 
             int[] maxthree = new int[] {int.MinValue, int.MinValue, int.MinValue};
+            int[][] basinCounts = new int[maxrow][];
+            for(int row = 0; row < maxrow; row++) {
+                basinCounts[row] = new int[maxcol];
+                for(int col = 0; col < maxcol; col++)
+                    basinCounts[row][col] = 0;
+            }
+
+            List<(int,int)> seen = new List<(int, int)>();
             for(int row = 0; row < maxrow; row++) {
                 for(int col = 0; col < maxcol; col++) {
                     if (PointRisk(grid, row, col) > 0) // it's a low point
                     {
-                        int basinSize = FindBasin(grid,row,col);
-
+                        var basinPoints = FindBasin(grid,row,col, seen);
+                        int basinSize = basinPoints.Count;
+                        foreach((var x, var y) in basinPoints) basinCounts[x][y] += 1;
                         if (basinSize > maxthree[0]) {
                             maxthree[2] = maxthree[1];
                             maxthree[1] = maxthree[0];
@@ -71,34 +80,45 @@ namespace day9
                 }
             }
 
+            for(int row = 0; row < maxrow; row++) {
+                for (int col = 0; col < maxcol; col++) {
+                    if (grid[row][col] < 9 && basinCounts[row][col] == 0)
+                        Console.WriteLine($"Point ({row},{col}) wasn't included in any basins.");
+                    if (grid[row][col] < 9 && basinCounts[row][col] > 1)
+                        Console.WriteLine($"Point ({row},{col}) was included in {basinCounts[row][col]} different basins.");
+                }
+            }
             Console.Out.WriteLine($"Top three were {maxthree[0]}, {maxthree[1]} and {maxthree[2]}");
             Console.Out.WriteLine($"Total basin sum is {maxthree[0] * maxthree[1] * maxthree[2]}");
         }
 
-        private static int FindBasin(int[][] grid, int row, int col)
+        private static HashSet<(int,int)> FindBasin(int[][] grid, int row, int col, List<(int,int)> seen)
         {
-            var result = BasinPoints(grid, row, col, 1, -1);
-            result.UnionWith(BasinPoints(grid, row, col, 1, 1));
-            result.UnionWith(BasinPoints(grid, row, col, -1, 1));
-            result.UnionWith(BasinPoints(grid, row, col, -1, -1));
+            var result = BasinPoints(grid, row, col, seen);
 
             Console.Out.WriteLine($"Point {row} {col} has a basin size of {result.Count}");
-            return result.Count;
+            return result;
         }
 
-        static HashSet<(int,int)> BasinPoints(int[][] grid, int row, int col, int rowd, int cold) {
+        static HashSet<(int,int)> BasinPoints(int[][] grid, int row, int col, List<(int,int)> seen) {
             var result = new HashSet<(int,int)>();
             if (row < 0 || col < 0 || row >= grid.Length || col >= grid[row].Length)
                 return result;
             if (grid[row][col] == 9)
                 return result;
+            if (seen.Contains((row,col)))
+                return result;
             result.Add((row,col));
-            var dirone = BasinPoints(grid, row + rowd, col, rowd, cold);
-            var dirtwo = BasinPoints(grid, row, col + cold, rowd, cold);
-            //var dirthr = BasinPoints(grid, row + rowd, col + cold, rowd, cold);
+            seen.Add((row, col));
+            var dirone = BasinPoints(grid, row - 1, col, seen);
+            var dirtwo = BasinPoints(grid, row, col + 1, seen);
+            var dirthree = BasinPoints(grid, row + 1, col, seen);
+            var dirfour = BasinPoints(grid, row, col - 1, seen);
 
             result.UnionWith(dirone);
             result.UnionWith(dirtwo);
+            result.UnionWith(dirthree);
+            result.UnionWith(dirfour);
 
             return result;
         }
